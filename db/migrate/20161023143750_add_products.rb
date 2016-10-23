2c92f16d197e58ebde860be38c736951f84adb32
kuznetz <s.kuznetzov@mail.ru>
2016-10-23 22:06:14 +0300

    вывод

--- db/migrate/20161023143750_add_products.rb
+++ db/migrate/20161023143750_add_products.rb
@@ -1,10 +1,34 @@
 class AddProducts < ActiveRecord::Migration
   def change
 
-  	Product.create :title => 'ipizza1', :description => 'pizza', :price => 350, :size =>30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/image/ ipizza1.jpg'
-
-     Product.create  :title => 'pizza2', :description => 'italian pizza', :price => 450, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/image/pizza2.jpg'
-
-     Product.create  :title => 'pizza3', :description => 'russia pizza', :price => 300, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/image/pizza3.jpg'
+  	Product.create ({
+    :title => 'ipizza1', 
+    :description => 'pizza', 
+    :price => 350, :size =>30, 
+    :is_spicy => false, 
+    :is_veg => false, 
+    :is_best_offer => false,
+     :path_to_image => '/image/ ipizza1.jpg'
+      })
+     Product.create  ({
+     :title => 'pizza2', 
+     :description => 'italian pizza',
+      :price => 450, 
+      :size => 30, 
+      :is_spicy => false,
+       :is_veg => false, 
+       :is_best_offer => true, 
+       :path_to_image => '/image/pizza2.jpg'
+      })
+     Product.create  ({
+     :title => 'pizza3', 
+     :description => 'russia pizza', 
+     :price => 300,
+     :size => 30, 
+     :is_spicy => false,
+      :is_veg => false, 
+      :is_best_offer => false, 
+      :path_to_image => '/image/pizza3.jpg'
+      })
   end
 end
--- public/app.js
+++ public/app.js
@@ -2,9 +2,9 @@
 
 function something()
 {
-	var x = window.localStorage.getItem('ccc');
+	var x = window.localStorage.getItem('ccc');//берём х из хэша и увеличиваем на 1
 
-    x = x *1 + 1
+    x = x *1 + 1;
 
 	window.localStorage.setItem('ccc',x);
 
