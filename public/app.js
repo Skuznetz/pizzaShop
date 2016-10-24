19b40812aee72cdb7160ca502959a95c8e70c00e
kuznetz <s.kuznetzov@mail.ru>
2016-10-24 17:53:29 +0300

    счетчик js

--- public/app.js
+++ public/app.js
@@ -19,3 +19,20 @@ var x = window.localStorage.getItem(key);
 x = x *1 + 1;
 window.localStorage.setItem(key,x);
 }
+
+function cart_get_number_of_items()
+{
+
+	var cnt = 0;
+	for(var i = 0;i < window.localStorage.length; i++)
+{
+var key = window.localStorage.key(i);// получаем ключ
+var value = window.localStorage.getItem(key);// получаем значение,аналог в руби: hh['ccc']= x
+
+if(key.indexOf('product_')== 0)
+{
+cnt = cnt + value * 1;
+}
+}
+return cnt;
+}
