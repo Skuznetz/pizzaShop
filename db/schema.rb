b03552a8016890baaa517181ccdba6f3c46c9252
kuznetz <s.kuznetzov@mail.ru>
2016-10-26 17:47:20 +0300

    переносим инициализацию в layout.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023143750) do
+ActiveRecord::Schema.define(version: 20161026115429) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
