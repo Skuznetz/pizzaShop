b260a09fd695dfdb7e2211ee9001364f99e7fb18
kuznetz <s.kuznetzov@mail.ru>
2016-10-28 21:12:56 +0300

    рефакторинг

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ erb :order_placed
 end
 
 post '/cart' do
-	@orders_input = params[:orders]
+	@orders_input = params[:orders_input]
 	 @items = parse_orders_input @orders_input
 	 @items.each do |item|
 	 	item[0] = Product.find(item[0])
